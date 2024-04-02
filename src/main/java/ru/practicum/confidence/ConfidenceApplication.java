f118d8437ed7945135f34a9fe1da249f36bd792a
Денис <Denis3010903@gmail.com>
2024-04-02 16:19:26 +0300

    Initial commit

---
+++ src/main/java/ru/practicum/confidence/ConfidenceApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.confidence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfidenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfidenceApplication.class, args);
+    }
+
+}
